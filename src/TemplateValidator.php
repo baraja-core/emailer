163a4e521fb81e510208c3c59cafd67a3fb2087f
Jan Barášek <janbarasek@gmail.com>
2021-10-13 14:14:01 +0200

    Validator: Add new validator service.

---
+++ src/TemplateValidator.php
@@ -0,0 +1,47 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Baraja\Emailer;
+
+
+final class TemplateValidator
+{
+	private const PATTERNS = [
+		'latte' => '~{\$([a-zA-Z0-9_]+)~',
+		'twig' => '{{\s*([a-zA-Z0-9_]+)',
+		'txt' => '{{\s*([a-zA-Z0-9_]+)',
+	];
+
+
+	/**
+	 * @param array<int, string> $params
+	 */
+	public function isAllParametersIncluded(string $content, string $format, array $params): bool
+	{
+		$hydratedParameters = $this->hydrateParameters($content, $format);
+		foreach ($params as $param) {
+			if (in_array($param, $hydratedParameters, true) === false) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+
+	/**
+	 * @return array<int, string>
+	 */
+	public function hydrateParameters(string $content, string $format): array
+	{
+		if (
+			isset(self::PATTERNS[$format])
+			&& preg_match_all(self::PATTERNS[$format], $content, $parser) > 0
+		) {
+			return $parser[1] ?? [];
+		}
+
+		return [];
+	}
+}
